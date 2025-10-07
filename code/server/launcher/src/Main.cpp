d619474b6830d1bd64dc80a6282f9e1a6f615d5e
Shi-Lan <vidllqs@gmail.com>
2025-10-07 20:21:46 +0200

    Update Main.cpp

--- code/server/launcher/src/Main.cpp
+++ code/server/launcher/src/Main.cpp
@@ -1,4 +1,4 @@
-z/*
+/*
  * This file is part of the CitizenFX project - http://citizen.re/
  *
  * See LICENSE and MENTIONS in the root of the source tree for information
@@ -122,4 +122,5 @@ int main(int argc, char* argv[])
 	return utf8main(argc, argv);
 }
 #endif
+
 
